4e8389ff6d1cc6422674b7eea6103e71984712ea
김진영C <jin0kim326@douzone.com>
2022-11-20 16:58:46 +0900

    delete and checkbox

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import { BsTrashFill } from 'react-icons/bs';
+import style from './TodoList.module.css';
+import Todo from '../Todo/Todo';
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
@@ -11,11 +14,24 @@ export default function TodoList() {
     setTodos([...todos, todo]);
   };
 
+  const handleUpdate = (updated) => {
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  };
+
+  const handleDelete = (deleted) => {
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+  };
+
   return (
     <>
       <ul>
         {todos.map((item) => (
-          <li key={item.id}> {item.text} </li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
         ))}
       </ul>
 
