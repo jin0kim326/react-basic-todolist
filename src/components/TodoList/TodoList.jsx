a47c119015c4f6162b916309e36d8d0d8420dcf6
김진영C <jin0kim326@douzone.com>
2022-11-21 18:02:24 +0900

    필터링하기

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,7 +4,7 @@ import { BsTrashFill } from 'react-icons/bs';
 import style from './TodoList.module.css';
 import Todo from '../Todo/Todo';
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: '1', text: '장보기', status: 'active' },
     { id: '2', text: '공부하기', status: 'active' },
@@ -22,10 +22,12 @@ export default function TodoList() {
     setTodos(todos.filter((t) => t.id !== deleted.id));
   };
 
+  const filtered = getFilteredItems(todos, filter);
+
   return (
     <>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -38,4 +40,11 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === 'all') {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
