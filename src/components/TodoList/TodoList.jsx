6e00242d868c9fced372e06f33b0345abb3e5b98
김진영C <jin0kim326@douzone.com>
2022-11-25 16:58:40 +0900

    bug 수정

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -4,7 +4,12 @@ import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState(readTodosFromLs());
+  // const [todos, setTodos] = useState(readTodosFromLs());
+  /**
+   * 위 코드는 상태가 변할때마다 readTodosFromLs()함수를 호출한다.
+   * 물론 값이 변하지는 않는다. 왜냐하면 리액트 내부에서 저장된 값이있으면 그값을 불러오기 때문에 초기값으로 갱신되지않음
+   *콜백으로 넘겨주면 처음에만 호출됨
+   */
   const [todos, setTodos] = useState(() => readTodosFromLs());
 
   useEffect(() => {
